9976b2873909b69422a66340677a13ab878ae799
rex0722 <rx800722@gmail.com>
2018-09-11 16:59:46 +0800

    Test

--- app/src/main/java/com/study/application/leanCloud/ActivityID.java
+++ app/src/main/java/com/study/application/leanCloud/ActivityID.java
@@ -1,6 +1,7 @@
 package com.study.application.leanCloud;
 
 public class ActivityID {
+//    Hi
     public static final int BORROW_ACTIVITY = 20001;
     public static final int RETURN_ACTIVITY = 20002;
     public static final int SUBSCRIBE_ACTIVITY = 20003;
